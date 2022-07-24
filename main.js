48abe7dc886b1150c9802075315f69c051b6dc2f
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2022-07-24 01:49:27 -0300

    iterando-flip-card

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const baseURL = "https://pokeapi.co/api/v2";
 const caja = document.querySelector("#caja");
 
-const cantidadPokemones = 15;
+const cantidadPokemones = 16;
 
 const fetchPokemones = async () => {
   for (let i = 1; i <= cantidadPokemones; i++) {
@@ -19,14 +19,20 @@ const getPokemon = async (id) => {
     console.log(`tenemos un error ${error}`);
   }
 };
-
+//<img src="${sprites.back_default}" />
 const pintarPoke = (pokemon) => {
   const div = document.createElement("div");
   div.classList.add("poke");
+  const flipCard = document.createElement('div');
+  flipCard.classList.add("flip-card");
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add("card-container");
+  flipCard.appendChild(cardContainer);
   const { id, name, sprites, height, weight, types } = pokemon;
   const pokeInnerHtml = `
     <img src="${sprites.front_default}" />
     <h2>${name}</h2>
+    <span>EXP: ${pokemon.base_experience}</span>
     <div class="tipo-poke">
     ${types
       .map((tipo) => {
@@ -40,7 +46,16 @@ const pintarPoke = (pokemon) => {
     <p class="weight">Weight:${weight}Kg</p>
     `;
   div.innerHTML = pokeInnerHtml;
-  caja.appendChild(div);
+
+  const cardBack = document.createElement('div');
+  cardBack.classList.add('pokemon-block-back');
+  cardBack.textContent = "carta de atras";
+
+  cardContainer.appendChild(div);
+  cardContainer.appendChild(cardBack);
+  caja.appendChild(flipCard);
 };
 
 fetchPokemones();
+
+
